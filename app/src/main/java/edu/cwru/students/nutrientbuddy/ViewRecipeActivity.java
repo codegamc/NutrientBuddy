e81e49f0e18a6e21d6a2fdad78ee10d6646a8caf
Liz Trinnes <liz@Lizs-MacBook.local>
2017-11-14 07:08:31 -0500

    Added text UI to RecipeView activity to display passed info.

--- app/src/main/java/edu/cwru/students/nutrientbuddy/ViewRecipeActivity.java
+++ app/src/main/java/edu/cwru/students/nutrientbuddy/ViewRecipeActivity.java
@@ -7,10 +7,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class ViewRecipeActivity extends AppCompatActivity {
 
     private static final String TAG = "MyActivity";
+    private TextView recipeNameText;
+    private TextView recipeIngrText;
+    private TextView recipeDirText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +33,27 @@ public class ViewRecipeActivity extends AppCompatActivity {
             }
         });
 
+        recipeNameText = (TextView) findViewById(R.id.recipe_name_received);
+        recipeIngrText = (TextView) findViewById(R.id.recipe_ingr_received);
+        recipeDirText = (TextView) findViewById(R.id.recipe_dir_received);
+
 
         String s = getIntent().getStringExtra("recipeName");
         Log.v(TAG, "Inside ViewRecipeActivity. Recipe name = " + s);
 
+        recipeNameText.setText(s);
+
         s = getIntent().getStringExtra("recipeIngredients");
         Log.v(TAG, "Inside ViewRecipeActivity. Recipe ingredients = " + s);
 
+        recipeIngrText.setText(s);
+
         s = getIntent().getStringExtra("recipeDirections");
         Log.v(TAG, "Inside ViewRecipeActivity. Recipe directions = " + s);
+
+        recipeDirText.setText(s);
+
+
 
     }
 
