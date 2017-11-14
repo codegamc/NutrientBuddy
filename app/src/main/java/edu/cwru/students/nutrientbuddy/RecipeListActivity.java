21c9d6eb5ddd196073cc3d83884a39c6adb62fea
Liz Trinnes <liz@Lizs-MacBook.local>
2017-11-13 21:15:55 -0500

    Navigate to EditRecipeActivity on button selection

--- app/src/main/java/edu/cwru/students/nutrientbuddy/RecipeListActivity.java
+++ app/src/main/java/edu/cwru/students/nutrientbuddy/RecipeListActivity.java
@@ -1,12 +1,28 @@
 package edu.cwru.students.nutrientbuddy;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 public class RecipeListActivity extends AppCompatActivity {
 
     private RecipeList list;
@@ -14,7 +30,8 @@ public class RecipeListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_list);
+        setContentView(R.layout.activity_recipe_list);
+        Button button = (Button)findViewById(R.id.button_send);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -22,13 +39,18 @@ public class RecipeListActivity extends AppCompatActivity {
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+    }
+
+    public void addRecipe(View view) {
+        Intent intent = new Intent(this, EditRecipeActivity.class);
+        startActivity(intent);
     }
 }
