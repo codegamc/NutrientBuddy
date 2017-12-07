835014ce5835284ee7f352cd46ac3a0ac50f03da
Liz Trinnes <liz@Lizs-MacBook.local>
2017-12-07 14:44:52 -0500

    Added HelpActivity

---
+++ app/src/main/java/edu/cwru/students/nutrientbuddy/HelpActivity.java
@@ -0,0 +1,29 @@
+package edu.cwru.students.nutrientbuddy;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.icu.text.StringSearch;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class HelpActivity extends AppCompatActivity{
+
+}
